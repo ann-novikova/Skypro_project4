3c31419b7afa7acf20d23c68793bfdcc4fb7ee56
Anna Novikova <anna.novikova.dev@gmail.com>
2025-06-18 14:34:44 +0300

    new classes added

--- src/product.py
+++ src/product.py
@@ -18,7 +18,10 @@ class Product:
         return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
 
     def __add__(self, other: "Product") -> float:
-        return self.__price * self.quantity + other.__price * other.quantity
+        if type(other) is self.__class__:
+            return self.__price * self.quantity + other.__price * other.quantity
+        else:
+            raise TypeError
 
     @classmethod
     def new_product(cls, new_product: dict) -> "Product":
